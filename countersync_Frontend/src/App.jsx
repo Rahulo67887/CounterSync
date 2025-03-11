41420698ce881afee695eaf695b083b82f8a5f7d
Rahul <rc7662837@gmail.com>
2025-03-12 01:19:51 +0545

    initialized the frontend based on react and created the navbar

---
+++ countersync_Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AdminDashboard from "./components/adminDashboard";
+
+function App() {
+  return (
+    <>
+      <AdminDashboard />
+    </>
+  );
+}
+
+export default App;
