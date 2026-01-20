42d4e93c3c69934ddb24dd4b7878d266976b8a47
Rahul Choudhary <rc7662837@gmail.com>
2026-01-20 23:40:55 +0530

    added comments in routes

--- countersync_Backend/Routes/deskRoutes.js
+++ countersync_Backend/Routes/deskRoutes.js
@@ -1,13 +1,15 @@
-const express=require("express");
-const router=express.Router();
+const express=require("express"); // Import Express framework
+const router=express.Router(); // Create a new router
 
-const {createDesk, loginDesk}=require("../Controllers/deskControllers");
+const {createDesk, loginDesk}=require("../Controllers/deskControllers");  // Import controller functions for desk operations
 
-const validate=require("../middlewares/validate_Middleware");
+const validate=require("../middlewares/validate_Middleware");  // Import validation middleware
+
+// Import Zod schemas for request validation
 const validate_loginSchema=require("../validators/validate_loginSchema");
 const validate_signupSchema=require("../validators/validate_signupSchema");
 
-router.post("/create", validate(validate_signupSchema), createDesk);
-router.post("/login", validate(validate_loginSchema), loginDesk);
+router.post("/create", validate(validate_signupSchema), createDesk);  // Route to create a new desk (signup)
+router.post("/login", validate(validate_loginSchema), loginDesk);  // Route to login an existing desk
 
 module.exports=router;
