fd3c73932aa7bf286c5e036848710819af52df8c
Rahul Choudhary <rc7662837@gmail.com>
2026-01-20 23:38:12 +0530

    added comments in validotors

--- countersync_Backend/validators/validate_loginSchema.js
+++ countersync_Backend/validators/validate_loginSchema.js
@@ -1,12 +1,13 @@
-const{z}=require("zod");
+const{z}=require("zod");  // Import zod for schema-based validation
 
-const loginSchema=z.object({
-    deskName: z
+const loginSchema=z.object({  // Define schema for login request validation
+    deskName: z  // deskName validation
     .string({ required_error: "Desk Name is required" })
     .trim()
     .min(3, { message: "Desk Name should at least contain 3 letters" })
     .max(20, { message: "Desk Name should at most contain 20 letters" }),
-  password: z
+
+  password: z // password validation
     .string({ required_error: "Password is required" })
     .trim()
     .min(8, { message: "Password should at least contain 8 letters" })
--- countersync_Backend/validators/validate_signupSchema.js
+++ countersync_Backend/validators/validate_signupSchema.js
@@ -1,12 +1,13 @@
-const {z}=require("zod");
+const {z}=require("zod");  // Import Zod for schema-based input validation
 
-const signupSchema=z.object({
-    deskName: z
+const signupSchema=z.object({ // Define validation schema for user signup
+    deskName: z  // deskName field validation rules
     .string({ required_error: "Desk Name is required" })
     .trim()
     .min(3, { message: "Desk Name should at least contain 3 letters" })
     .max(20, { message: "Desk Name should at most contain 20 letters" }),
-  password: z
+
+  password: z   // password field validation rules
     .string({ required_error: "Password is required" })
     .trim()
     .min(8, { message: "Password should at least contain 8 letters" })
